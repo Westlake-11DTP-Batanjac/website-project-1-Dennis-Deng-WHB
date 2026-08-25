76dece535951f1f86e0fef3b63bbd6f9778dd968
Dennis Deng <zd24152@my.westlake.school.nz>
2026-08-25 14:19:28 +1200

    Assessment save 3

--- Python/Test.py
+++ Python/Test.py
@@ -9,34 +9,37 @@ def Begin():
 def Question1(score):
     Answer_set_1 = ["Christchurch", "Wellington", "Auckland"]
     Q_Num = 1
+    Answers = 1
     print(f"Question {Q_Num}: In what city is Karl Urban born?")
     for i in Answer_set_1:
-        print(f"{Q_Num}. {i}")
-        Q_Num += 1
+        print(f"{Answers}. {i}")
+        Answers += 1
     while True:
         try:
             answer = input("Enter your answer: ").lower().strip()
-            Check(answer, score)
+            Check(Q_Num, answer, score)
         except:
             print("An error occured, retry")
 
 def Question2(score):
     Question = ["1970", "1971", "1972"]
-    Q_Num = 1
+    Q_Num = 2
+    Answers = 1
     print(f"Question {Q_Num}: What year is Karl Urban born?")
     for i in Question:
-        print(f"{Q_Num}. {i}")
-        Q_Num += 1
+        print(f"{Answers}. {i}")
+        Answers += 1
     while True:
         try:
             answer = input("Enter your answer: ").lower().strip()
-            Check(answer, score)
+            Check(Q_Num, answer, score)
         except:
             print("An error occured, retry")
 
 def Question3(score):
     Question = ["Ghost Ship", "Lord of the Rings", "Star Trek"]
-    Q_Num = 1
+    Q_Num = 3
+    Answers = 1
     print(f"Question {Q_Num}: What's Karl Urban's first Hollywood film name?")
     for i in Question:
         print(f"{Answers}. {i}")
@@ -44,13 +47,14 @@ def Question3(score):
     while True:
         try:
             answer = input("Enter your answer: ").lower().strip()
-            Check(answer, score)
+            Check(Q_Num, answer, score)
         except:
             print("An error occured, retry")
 
 def Question4(score):
     Question = ["Thor: Ragnarok", "Iron Man 3", "Spiderman: Brand New Day"]
-    Q_Num = 1
+    Q_Num = 4
+    Answers = 1
     print(f"Question {Q_Num}: In what MARVEL movie did Karl Urban participate?")
     for i in Question:
         print(f"{Answers}. {i}")
@@ -58,13 +62,14 @@ def Question4(score):
     while True:
         try:
             answer = input("Enter your answer: ").lower().strip()
-            Check(answer, score)
+            Check(Q_Num, answer, score)
         except:
             print("An error occured, retry")
 
 def Question5(score):
     Question = ["Butcher", "Homelander", "A-Train"]
-    Q_Num = 1
+    Q_Num = 5
+    Answers = 1
     print(f"Question {Q_Num}: In The BOYS, who did Karl Urban act as?")
     for i in Question:
         print(f"{Answers}. {i}")
@@ -72,13 +77,14 @@ def Question5(score):
     while True:
         try:
             answer = input("Enter your answer: ").lower().strip()
-            Check(answer, score)
+            Check(Q_Num, answer, score)
         except:
             print("An error occured, retry")
 
 def Question6(score):
-    Question = ["Mortal Kombat II", "Mortal Kombat I", "Mortal Combat II"]
-    Q_Num = 1
+    Question = ["Mortal Kombat I", "Mortal Kombat II", "Mortal Combat II"]
+    Q_Num = 6
+    Answers = 1
     print(f"Question {Q_Num}: Which of the following movie is Karl in?")
     for i in Question:
         print(f"{Answers}. {i}")
@@ -86,33 +92,27 @@ def Question6(score):
     while True:
         try:
             answer = input("Enter your answer: ").lower().strip()
-            Check(answer, score)
+            Check(Q_Num, answer, score)
         except:
             print("An error occured, retry")
 
 def Question7(score):
-    Q_Num = 1
+    Q_Num = 7
     print(f"Question {Q_Num}: True or False: Karl Urban voiced the character, Bob, in Ark: The Animated Series")
-    for i in Question:
-        print(f"{Answers}. {i}")
-        Answers += 1
     while True:
         try:
             answer = input("Enter your answer: ").lower().strip()
-            Check(answer, score)
+            Check(Q_Num, answer, score)
         except:
             print("An error occured, retry")
 
 def Question8(score):
-    Q_Num = 1
+    Q_Num = 8
     print(f"Question {Q_Num}: True or False: Karl Urban played as Eomer in The Lord of the Rings: Return of the King")
-    for i in Question:
-        print(f"{Answers}. {i}")
-        Answers += 1
     while True:
         try:
             answer = input("Enter your answer: ").lower().strip()
-            Check(answer, score)
+            Check(Q_Num, answer, score)
         except:
             print("An error occured, retry")
 
@@ -123,48 +123,48 @@ def Check(Q_Num, answer, score):
             print(f"Correct! Score: {score}")
         else:
             print(f"Wrong! Score: {score}")
-    # elif Q_Num == 2:
-    #     if answer == "1972":
-    #         score += 1
-    #         print(f"Correct! Score: {score}")
-    #     else:
-    #         print(f"Wrong! Score: {score}")
-    # elif Q_Num == 3:
-    #     if answer == "ghostship":
-    #         score += 1
-    #         print(f"Correct! Score: {score}")
-    #     else:
-    #         print(f"Wrong! Score: {score}")
-    # elif Q_Num == 4:
-    #     if answer == "thor:ragnarok":
-    #         score += 1
-    #         print(f"Correct! Score: {score}")
-    #     else:
-    #         print(f"Wrong! Score: {score}")
-    # elif Q_Num == 5:
-    #     if answer == "butcher":
-    #         score += 1
-    #         print(f"Correct! Score: {score}")
-    #     else:
-    #         print(f"Wrong! Score: {score}")
-    # elif Q_Num == 6:
-    #     if answer == "mortalkombatii":
-    #         score += 1
-    #         print(f"Correct! Score: {score}")
-    #     else:
-    #         print(f"Wrong! Score: {score}")
-    # elif Q_Num == 7:
-    #     if answer == "true":
-    #         score += 1
-    #         print(f"Correct! Score: {score}")
-    #     else:
-    #         print(f"Wrong! Score: {score}")
-    # elif Q_Num == 8:
-    #     if answer == "true":
-    #         score += 1
-    #         print(f"Correct! Score: {score}")
-    #     else:
-    #         print(f"Wrong! Score: {score}")
+    elif Q_Num == 2:
+        if answer == "1972":
+            score += 1
+            print(f"Correct! Score: {score}")
+        else:
+            print(f"Wrong! Score: {score}")
+    elif Q_Num == 3:
+        if answer == "ghostship":
+            score += 1
+            print(f"Correct! Score: {score}")
+        else:
+            print(f"Wrong! Score: {score}")
+    elif Q_Num == 4:
+        if answer == "thor:ragnarok":
+            score += 1
+            print(f"Correct! Score: {score}")
+        else:
+            print(f"Wrong! Score: {score}")
+    elif Q_Num == 5:
+        if answer == "butcher":
+            score += 1
+            print(f"Correct! Score: {score}")
+        else:
+            print(f"Wrong! Score: {score}")
+    elif Q_Num == 6:
+        if answer == "mortalkombatii":
+            score += 1
+            print(f"Correct! Score: {score}")
+        else:
+            print(f"Wrong! Score: {score}")
+    elif Q_Num == 7:
+        if answer == "true":
+            score += 1
+            print(f"Correct! Score: {score}")
+        else:
+            print(f"Wrong! Score: {score}")
+    elif Q_Num == 8:
+        if answer == "true":
+            score += 1
+            print(f"Correct! Score: {score}")
+        else:
+            print(f"Wrong! Score: {score}")
 
 def End(score):
     print("------Test Over------")
